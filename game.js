ac47fa51fe53aa9aacd5e0b85472871d74141d91
Kristina <kashqar1117@gmail.com>
2020-12-02 10:36:05 -0600

    added css to game

---
+++ game.js
@@ -0,0 +1,5 @@
+var questions = document.querySelector('#question')
+var choices = document.querySelectorAll('.choice-text')
+var questions = document.querySelector('question')
+
+var questions = ['Whats the biggest animal on the planet?', '']
