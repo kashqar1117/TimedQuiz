ce0ee01c8f90804763823313fd1d5d4c19685867
Kristina <kashqar1117@gmail.com>
2020-12-02 11:33:26 -0600

    saving javascript beofr exp

--- game.js
+++ game.js
@@ -2,4 +2,96 @@ var questions = document.querySelector('#question')
 var choices = document.querySelectorAll('.choice-text')
 var questions = document.querySelector('question')
 
-var questions = ['Whats the biggest animal on the planet?', '']
+let questions = [
+    {question: 'whatis 2+2?',
+    choice:'2',
+    choice2:'3',
+    choice3:'4',
+    choice4:'5',
+    answer: 3,
+    
+
+} ,
+{question: 'whatis 2+0?',
+choice:'2',
+choice2:'3',
+choice3:'4',
+choice4:'5',
+answer: 1,
+
+
+} ,
+{question: 'whatis 2+1?',
+choice:'2',
+choice2:'3',
+choice3:'4',
+choice4:'5',
+answer: 2,
+
+
+} ,
+{question: 'whatis 2+3?',
+choice:'2',
+choice2:'3',
+choice3:'4',
+choice4:'5',
+answer: 4,
+
+
+} 
+]
+
+function startGame(event){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const SCORE_POINTS = 100
+// const max_questions=4
+
+// function startGame()
+// {
+//     questionCounter = 0;
+//     score = 0;
+//     availableQuestions = [...questions];
+//     getNewQuestions();
+
+
+// }
+
+// getNewQuestions(){
+//     if (availableQuestions.lenth===0 || questionCounter >max_questions)
+//     localStorage.setItem('score', score)
+//     return window.location.assign('/end.html')
+
+
+//    questionCounter++
+//    progresstext.innerText 
+// }
